73712977e2d68fcca1dbe231d9fa28a469ef19e6
Faris Abdelrahem <farishabdelrahem@gmail.com>
2024-10-05 01:28:12 -0400

    Modified analyzer and requirements to remove tokenizer as it was too slow and did not accomplish much, also adjusted threshold for a prompt to be considered emotionally complex

--- AIOrchestrator/analyzer.py
+++ AIOrchestrator/analyzer.py
@@ -1,11 +1,6 @@
 # analyzer.py is a module that analyzes the complexity of a given text
-import nltk
 from transformers import pipeline
-from nltk.tokenize import sent_tokenize, word_tokenize
 
-# Ensure the necessary NLTK resources are downloaded
-nltk.download('punkt')
-nltk.download('punkt_tab')
 
 # Loads model for emotion classification
 try:
@@ -19,16 +14,15 @@ def isComplex(text):
 
     # Consider the text complex if the model detects anger, fear or sadness
     labelComplex = ["anger", "fear", "sadness"]
-    emotionallyComplex = any(label['label'] in labelComplex for label in result)
+    emotionallyComplex = any(label['label'] in labelComplex and label['score'] > 0.67 for label in result)
 
     # Tokenize text into sentences
-    sentences = sent_tokenize(text)
+    words = text.split(" ")
+    print(f'result: {result}')
 
-    # Calculate average sentence length
-    avgSentLen = sum(len(word_tokenize(sentence)) for sentence in sentences)/len(sentences)
-
-    # Consider the text complex if the average sentence length is greater than 20
-    lengthComplex = avgSentLen > 20
+    # Consider the text complex if there are over 20 sentences
+    lengthComplex = len(words) > 100
 
     # Return true if text is complex either emotionally or length wise
     return emotionallyComplex or lengthComplex
+
