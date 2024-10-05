3422f17a6e62357750ba5a869e7f797cfdcb4a7f
electricStove <144375550+electricStove@users.noreply.github.com>
2024-10-05 18:09:32 -0400

    Context Caching is working.

--- AIOrchestrator/orchestra.py
+++ AIOrchestrator/orchestra.py
@@ -6,19 +6,22 @@ from sys import path
 # Add the path to the parent directory
 path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from AdvanceAI.AdvanceAI import advance_generate_response
+from AdvanceAi.AdvanceAI import advance_generate_response
 from tts.text_to_speech import text_to_speech
 from Roberta.roberta import roberta_response
 
 context_dict: dict[str, dict[str, list[str]]] = {}
 
 def orchestrate(usrPrompt: str, caller_id):
-    # If this is an ongoing
+    # If this is a new call, create the context for this caller
     if caller_id not in context_dict:
         context_dict[caller_id] = {
             "user": [],
             "assistant": []
         }
+
+    # Adds the current user prompt to the context
+    context_dict[caller_id]["user"].append(usrPrompt)
 
     response = advance_generate_response(usrPrompt, context_dict[caller_id])
 
@@ -35,7 +38,7 @@ def orchestrate(usrPrompt: str, caller_id):
     except json.JSONDecodeError:
         pass
 
-    context_dict[caller_id]["user"].append(usrPrompt)
+    # Store the new response as an assistant response in the context
     context_dict[caller_id]["assistant"].append(response)
 
     # Generates an audio file from the response to be used in the call
@@ -44,5 +47,10 @@ def orchestrate(usrPrompt: str, caller_id):
 
 if __name__ == "__main__":
     orchestrate(
-        "Hi my name is Ballard Alice my pin is 1234 and I would like to know my account balance."
+        "My name is Alice and my pin is 1234 and I would like to check my balance",
+        "4691234445"
+    )
+    orchestrate(
+        "What was my last question",
+        "4691234445"
     )
