7c315c378939011a10aaeb18fc80e34221d74cdd
Faris Abdelrahem <farishabdelrahem@gmail.com>
2024-10-04 23:46:04 -0400

    Removed uncessary files from orchestrator, utilizes generate_response from advanced_gpt

--- AIOrchestrator/orchestra.py
+++ AIOrchestrator/orchestra.py
@@ -1,5 +1,16 @@
 # the main file for the AI Orchestrator module
 
 from analyzer import isComplex
+from advanced_gpt.generate_response import generate_response
+
+def orchestrate(usrPrompt: str):
+    response = ""
+    if isComplex(usrPrompt):
+        response = generate_response(usrPrompt)
+    else:
+        print("GPT 2 is not setup yet, utiilizing GPT 4...")
+        response = generate_response(usrPrompt)
+
+      
 
 
