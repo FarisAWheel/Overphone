5dc11fa576c72da61501b553c91e41c8a3a521c0
Faris Abdelrahem <farishabdelrahem@gmail.com>
2024-10-05 04:28:33 -0400

    Fixed filename issues in orchestra.py

--- AIOrchestrator/orchestra.py
+++ AIOrchestrator/orchestra.py
@@ -1,9 +1,9 @@
 # The main file for the AI Orchestrator module
-from AdvanceAI import generate_response
+from AdvanceAI import advance_generate_response
 from tts.text_to_speech import text_to_speech
 
 def orchestrate(usrPrompt: str):
-    response = generate_response(usrPrompt)
+    response = advance_generate_response(usrPrompt)
 
     # Genereates an audio file from the response to be used in the call
     return text_to_speech(response)
